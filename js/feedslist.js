a61f3327497e3591a0447c4df2db132379e30912
christopherpeter <christopher@tychons.com>
2015-06-08 13:43:47 +0530

    latest commit

---
+++ js/feedslist.js
@@ -0,0 +1,38 @@
+﻿function loadfeeds()
+{
+    var questions = Parse.Object.extend("questionare");
+    var query = new Parse.Query(questions);
+
+    query.equalTo("userid", Parse.User.current().id);
+    query.find({
+        success: function (questions) {
+            var output = "";
+            for (var i = 0; i < questions.length; i++) {
+                var object = questions[i];
+
+                output = output + ' <div class="panel panel-default">';
+                output = output + ' <div class="panel-heading"><a onclick=deletequestion("' + object.id + '") style="margin-left:15px"  class="pull-right">Delete</a><a href="editquestion.html?' + object.id + '" class="pull-right">Edit</a> <h4>Question ' + (i + 1) + '</h4></div>';
+                output = output + ' <div class="panel-body">';
+                output = output + ' <span>' + object.get('question') + '</span>';
+                output = output + ' <div class="clearfix"></div>';
+                output = output + ' <hr>';
+                output = output + "<p>Option A -<label>" + object.get('option1') + "</label></p>"
+                output = output + "<p>Option B -<label>" + object.get('option2') + "</label></p>"
+                output = output + "<p>Option C -<label>" + object.get('option3') + "</label></p>"
+                output = output + "<p>Option D -<label>" + object.get('option4') + "</label></p>"
+                output = output + ' <hr>'
+                output = output + ' <p>Correct Answer:' + object.get('correctanswer') + '</p>';
+                output = output + ' </div>';
+                output = output + ' </div>';
+
+            }
+
+            $("#questions").html(output);
+        },
+        error: function (object, error) {
+            // The object was not retrieved successfully.
+            // error is a Parse.Error with an error code and message.
+            alert("Error: " + error.code + " " + error.message);
+        }
+    });
+}
